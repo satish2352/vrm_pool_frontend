69eb972507bf5cf82fdca3ecf1a0bff7377ed6dc
ravikandekar <153173943+ravikandekar@users.noreply.github.com>
2024-05-08 14:13:20 +0530

    ...

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     // BASE_URL:'http://35.154.44.56:3000/api', //SMG SERVER
-    BASE_URL:'http://35.154.44.56:3000/api',
+    BASE_URL:'https://35.154.44.56:3000/api',
 
     
    
