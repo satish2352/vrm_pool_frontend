af056dbcf47d5397d7c1bf6b939fb95f5ba95249
ravikandekar <153173943+ravikandekar@users.noreply.github.com>
2024-05-08 14:36:05 +0530

    env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,7 +8,7 @@ function getBaseUrl() {
 
   // Define the base URLs based on different server IPs
   const baseUrls:any = {
-    '35.154.44.56': 'https://35.154.44.56:3000/api', //VRM UAT
+    '35.154.44.56': 'http://35.154.44.56:3000/api', //VRM UAT
     '3.111.214.34': 'http://3.111.214.34:3000/api' //SMG
     // Add more IP to URL mappings as needed  
   };
