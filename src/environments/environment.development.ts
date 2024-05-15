3efeb7d2153eaf8f5638d37a02e0c76a18b704d0
ravikandekar <153173943+ravikandekar@users.noreply.github.com>
2024-05-15 18:50:08 +0530

    env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -19,14 +19,15 @@ function getBaseUrl() {
   // Define the base URLs based on different server IPs
   const baseUrls:any = {
     // '35.154.44.56': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
-    'https://stargatestaging-poolmonitor.exotel.com/login': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
+    // '35.154.44.56': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
+    'https://stargatestaging-poolmonitor.exotel.com:3000/login': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
     '3.111.214.34': 'http://3.111.214.34:3000/api' //SMG
     
   };
 
   // Default base URL if server IP does not match any defined mappings
   // const defaultBaseUrl = 'http://35.154.44.56:3000/api';
-  const defaultBaseUrl = 'https://stargatestaging-poolmonitor.exotel.com/login';
+  const defaultBaseUrl = 'https://stargatestaging-poolmonitor.exotel.com:3000/login';
 
   // Return the corresponding base URL based on the server's IP
   return baseUrls[serverIp] || defaultBaseUrl;
