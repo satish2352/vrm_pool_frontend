5812ff4872bac1035554016f61b8231587f929ad
ravikandekar <153173943+ravikandekar@users.noreply.github.com>
2024-05-15 19:06:20 +0530

    ok

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -18,17 +18,17 @@ function getBaseUrl() {
 
   // Define the base URLs based on different server IPs
   const baseUrls:any = {
-    // '35.154.44.56': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
-    // '35.154.44.56': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
-    'https://stargatestaging-poolmonitor.exotel.com':'https://stargatestaging-poolmonitor.exotel.com:3000/api',
-    // 'https://stargatestaging-poolmonitor.exotel.com:3000/login': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
-    '3.111.214.34': 'http://3.111.214.34:3000/api' //SMG
+    // // '35.154.44.56': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
+    // // '35.154.44.56': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
+    // 'https://stargatestaging-poolmonitor.exotel.com':'https://stargatestaging-poolmonitor.exotel.com:3000/api',
+    // // 'https://stargatestaging-poolmonitor.exotel.com:3000/login': 'https://stargatestaging-poolmonitor.exotel.com:3000/api', //VRM UAT
+    // '3.111.214.34': 'http://3.111.214.34:3000/api' //SMG
     
   };
 
   // Default base URL if server IP does not match any defined mappings
   // const defaultBaseUrl = 'http://35.154.44.56:3000/api';
-  const defaultBaseUrl = 'https://stargatestaging-poolmonitor.exotel.com:3000';
+  const defaultBaseUrl = 'https://stargatestaging-poolmonitor.exotel.com:3000/api';
 
   // Return the corresponding base URL based on the server's IP
   return baseUrls[serverIp] || defaultBaseUrl;
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -8,13 +8,13 @@ function getBaseUrl() {
 
   // Define the base URLs based on different server IPs
   const baseUrls:any = {
-    'https://stargatestaging-poolmonitor.exotel.com':'https://stargatestaging-poolmonitor.exotel.com:3000/api',
-    '3.111.214.34': 'http://3.111.214.34:3000/api' //SMG
+    // 'https://stargatestaging-poolmonitor.exotel.com':'https://stargatestaging-poolmonitor.exotel.com:3000/api',
+    // '3.111.214.34': 'http://3.111.214.34:3000/api' //SMG
     // Add more IP to URL mappings as needed  
   };
 
   // Default base URL if server IP does not match any defined mappings
-  const defaultBaseUrl = 'https://stargatestaging-poolmonitor.exotel.com:3000';
+  const defaultBaseUrl = 'https://stargatestaging-poolmonitor.exotel.com:3000/api';
 
   // Return the corresponding base URL based on the server's IP
   return baseUrls[serverIp] || defaultBaseUrl;
